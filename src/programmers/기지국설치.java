e2b8486f4a4a21c131fd02ec1b6e651e42df16f9
이채은 <dlcodms123@gmail.com>
2020-05-29 21:27:57 +0900

    update

--- src/programmers/기지국설치.java
+++ src/programmers/기지국설치.java
@@ -9,49 +9,40 @@ public class 기지국설치 {
 	}
 	public static int solution(int n, int[] stations, int w) {
 		int answer = 0;
-		
+
 		int diff =0;
-		for(int i=0; i<stations.length; i++) {
+
+
+		diff = (stations[0]-w)-1;
+		System.out.println("diff:"+diff);
+		if(diff>0) {
+			answer += diff/(w*2+1);
+			if(diff%(w*2+1)!=0) answer++;
+		}
+		System.out.println("answer:"+answer);
+
+		for(int i=1; i<stations.length; i++) {
 			System.out.println(stations[i]);
-			if(i==0) {
-				if(stations[0]>1) {
-					diff = stations[0]-w-1;
-					answer += diff/(w*2+1);
-					if(diff%(w*2+1)!=0) answer++;
-					
-				}
-				//System.out.println("answer:"+answer);
-				
-				if(stations.length == 1) {
-					diff = n-(stations[i]+w);
-					if(diff<0) continue;
-					answer += diff/(w*2+1);
-					if(diff%(w*2+1)!=0) answer++;
-				}
-				//System.out.println("answer:"+answer);
-				
-				
-			}else if(i==stations.length-1) {
-				diff = (stations[i]-w)-(stations[i-1]+w)-1;
-				answer += diff/(w*2+1);
-				if(diff%(w*2+1)!=0) answer++;
-				
-				if(stations[i]<n) {
-					diff = n-(stations[i]+w);
-					if(diff<0) continue;
-					answer += diff/(w*2+1);
-					if(diff%(w*2+1)!=0) answer++;
-				}
-				
-			}else {
-				diff = (stations[i]-w)-(stations[i-1]+w)-1;
+
+			diff = (stations[i]-w)-(stations[i-1]+w)-1;
+			if(diff>0) {
 				answer += diff/(w*2+1);
 				if(diff%(w*2+1)!=0) answer++;
 			}
+
 			//System.out.println((int)(diff/(float)(2*w+1) + 1));
 			System.out.println("diff:"+diff);
 			System.out.println("answer:"+answer);
 		}
+
+		diff = n - (stations[stations.length-1]+w);
+		System.out.println("diff:"+diff);
+
+		if(diff>0) {
+			answer += diff/(w*2+1);
+			if(diff%(w*2+1)!=0) answer++;
+		}
+		System.out.println("answer:"+answer);
 
 		return answer;
 	}
