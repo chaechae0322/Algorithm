3b5984a26236c25c89aa4a8b7835a036a77822e4
chaechae0322 <dlcodms123@naver.com>
2020-09-27 00:52:44 +0900

    2020.09.27 트리의 순휘 - 재귀, 트리

--- Programmers/B2263_트리의순회.java
+++ Programmers/B2263_트리의순회.java
@@ -3,63 +3,57 @@ package Programmers;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.StringTokenizer;
-
+/*
+ * 재귀
+ * 트리 
+ * 너무 어렵게 푼듯.. 더 빠른방법이 있다
+ */
 public class B2263_트리의순회 {
-	static int n, in[], post[], info[][];
-	static List<Integer>[] g;
+	static int n, in[], post[], info[][], g[][];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer token;// = new StringTokenizer(br.readLine());
 		n=Integer.parseInt(br.readLine());
-		in=new int[n]; post=new int[n]; g=new ArrayList[n+1];
-		info=new int[n][2];
-		for(int i=0; i<=n; i++) g[i]=new ArrayList<>();
+		in=new int[n]; post=new int[n]; g=new int[n+1][2];
+		for(int i=0; i<=n; i++) {
+			g[i][0]=-1; g[i][1]=-1;
+		}
 		token = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++) {
 			in[i]=Integer.parseInt(token.nextToken());
-			info[i][0]=i; info[i][1]=in[i];
 		}
 		token = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++) {
 			post[i]=Integer.parseInt(token.nextToken());
 		}
-		Arrays.sort(info, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[1]-o2[1];
-			}
-		});
-		
-		solve(0, n-1);
+
+		idx=n;
+		solve(0, n-1, -1, -1);
 		preorder(post[n-1]);
 	}
 	private static void preorder(int x) {
+		if(x==-1) return;
 		visit(x);
-		for(int i=0; i<g[x].size(); i++) {
-			preorder(g[x].get(i));
-		}
+		preorder(g[x][0]);
+		preorder(g[x][1]);
 	}
 	private static void visit(int x) {
+		if(x==-1) return;
 		System.out.print(x+" ");
 	}
-	static int cnt;
-	private static int solve(int l, int r) {
-		cnt++;
-		if(cnt>=10) System.exit(0);
+	static int cnt, idx;
+	private static void solve(int l, int r, int p, int sel) {
+		if(l>r) return;
 		
-		int pivot = post[r];
-		System.out.println("l:"+l+" r:"+r+" pivot:"+pivot);
-		for(int i=l; i<r; i++) {
+		int pivot = post[--idx];
+		for(int i=l; i<=r; i++) {
 			if(in[i]==pivot) {
-				g[pivot].add(solve(l, i-1));
-				g[pivot].add(solve(i+1, r));
+				solve(i+1, r, pivot, 1);
+				solve(l, i-1, pivot, 0);
 			}
 		}
-		return in[l];
+		if(sel==0) g[p][0]=pivot;
+		else if(sel==1) g[p][1]=pivot;
 	}
 }
