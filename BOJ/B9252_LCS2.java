a185a08cf5dcbf228d5f968dfd1440011c99fb64
chaechae0322 <dlcodms123@naver.com>
2020-10-03 14:01:51 +0900

    2020.10.03 LCS2 Knap-sack

--- BOJ/B9252_LCS2.java
+++ BOJ/B9252_LCS2.java
@@ -1,14 +1,24 @@
 package BOJ;
-
-import java.util.Arrays;
 import java.util.Scanner;
-
+/*
+ * DP (knapsack)
+ * 
+ * 역추적
+ * dp값이 다르고 r[y]==p[x]일경우에 유효한값 주의
+ */
 public class B9252_LCS2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String r=sc.next();
 		String p=sc.next();
+        		
+		if(r.length()>p.length()) {
+			String t=r;
+			r=p;
+			p=t;
+		}
+        
 		int x=0,y=0, dp[][]=new int[r.length()+1][p.length()+1];
 		char[] ans=new char[1001];
 		for(int i=1; i<=r.length(); i++) { // 
@@ -21,25 +31,20 @@ public class B9252_LCS2 {
 				}
 			}
 		}
-		System.out.println("check");
-		for(int i=0; i<=r.length(); i++) {
-			for(int j=0; j<=p.length(); j++) {
-				System.out.print(dp[i][j]+" ");
-			}
-			System.out.println();
-		}
 		StringBuilder sb = new StringBuilder();
 		x=p.length(); y=r.length();
 		int idx=dp[y][x];
 		while(y>=0 && x>=0 && idx>0) {
 			while(y-1>=0 && dp[y][x]==dp[y-1][x]) y--;
-			//System.out.println("x:"+x+" y:"+y+" char:"+r.charAt(y-1));
-			
-			ans[--idx]=r.charAt(y-1);
-			x--; y--;
-			if(idx==0)break;
-			
+			if(r.charAt(y-1) == p.charAt(x-1)) {
+				ans[--idx]=r.charAt(y-1);
+				x--; y--;
+				if(idx==0) break;
+			}else {
+				x--;
+			}
 		}
+		
 		sb.append(ans, 0, dp[r.length()][p.length()]);
 		System.out.println(dp[r.length()][p.length()]);
 		if(sb.length()>0)System.out.println(sb.toString());
