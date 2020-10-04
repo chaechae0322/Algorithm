b0f1a2ab2405797100db58cb06a8ce3bdfeb4798
chaechae0322 <dlcodms123@naver.com>
2020-10-04 23:53:35 +0900

    2020.10.04 트리 - root에depth정보 포함해서 기록해야함

--- Programmers/Ntech/sol4.java
+++ Programmers/Ntech/sol4.java
@@ -1,16 +1,19 @@
-package Programmers;
+package Programmers.Ntech;
 
 import java.util.ArrayList;
-
+/*
+ * DSF, Tree
+ * root에 depth 정보까지 함께 표기
+ */
 public class sol4 {
 
 	public static void main(String[] args) {
-		int[][] t1= {{-1, 1}, {-1, 2},{-1,-1}};
-		int[][] t2=	{{-1, -1}};
-		
-		
+//		int[][] t1= {{-1, 1}, {-1, 2},{-1,-1}};
+//		int[][] t2=	{{-1, -1}};
 //		int[][] t1= {{1,2},{3,4},{5,6},{-1,7},{8,9},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
 //		int[][] t2=	{{1,2},{-1,-1},{-1,-1}};
+		int[][] t1= {{1,2},{3,-1},{-1,-1},{-1,4},{5,6},{-1,-1},{-1,-1}};
+		int[][] t2=	{{1,-1},{-1,2},{-1,3},{4,-1},{-1,-1}};
 		System.out.println(solution(t1, t2));
 
 	}
@@ -19,16 +22,16 @@ public class sol4 {
     public static int solution(int[][] t1, int[][] t2) {
         int answer = 0;
         r1=t1; r2=t2;
-
-        // t2 root 만들기
-        root2 = dfs(0, -1, 2);
+        // t2 root 만들기 
+        root2 = dfs(0, -1, 2, 0);
+        if(root2.length()==0) return t1.length; 
         
-        dfs(0, -1, 1);
+        dfs(0, -1, 1, 0);
         answer=ans;
         return answer;
     }
     static int ans;
-	private static String dfs(int pos, int c, int sel) {
+	private static String dfs(int pos, int c, int sel, int depth) {
 		StringBuilder s=new StringBuilder();
 		
 		int tmp=0;
@@ -37,11 +40,12 @@ public class sol4 {
 			else tmp=r2[pos][i];
 			if(tmp==-1) continue;
 			
-			s.append(dfs(tmp, i, sel));
+			s.append(dfs(tmp, i, sel, depth+1));
 			if(s.toString().equals(root2)) {
 				ans++;
 			}
 		}
+		s.append(depth);
 		if(c==0) s.append("L");
 		else if(c==1) s.append("R");
 		
