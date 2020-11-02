5f600cfb2891c6cec7b221b5d4561ec9b4863740
chaechae0322 <dlcodms123@naver.com>
2020-11-02 23:51:07 +0900

    Update B1509_팰린드롬분할.java

--- BOJ/B1509_팰린드롬분할.java
+++ BOJ/B1509_팰린드롬분할.java
@@ -6,48 +6,59 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class B1509_팰린드롬분할 {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String input = br.readLine();
+	int n = input.length();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input = br.readLine();
-		int m = input.length();
-		
-		int[] p = new int[m];
-		int[] dp = new int[m];
-
-		int idx=1;
-		p[0]=1;
-		
-		while(idx<m) {
-			if(idx-p[idx-1]-1>=0 && input.charAt(idx-p[idx-1]-1)==input.charAt(idx)) {
-				// 팰린드롬
-				p[idx]+=(p[idx-1]+2);
-			}else {
-				int tmp=1;
-				while(tmp<=idx && input.charAt(idx-tmp)==input.charAt(idx)) {
-					tmp++;
-				}
-				p[idx]=tmp;
-			}
-			idx++;
-		}
-		
-		// 팰린드롬 체크
-		System.out.println(Arrays.toString(p));
-		for(int i=0; i<m; i++) {
-			if(p[i]>1) {
-				System.out.println(input.substring(i-p[i]+1,i+1));
-			}
-		}
-		dp[0]=p[0];
-		for(int i=1; i<m; i++) {
-			if(i-p[i]<0) {
-				dp[i]=Math.min(1, dp[i-1]+1);
-			}else {
-				dp[i]=Math.min(dp[i-p[i]]+1, dp[i-1]+1);
-			}
-		}
-		System.out.println(dp[m-1]);
+	int[] dp=new int[n]; // 
+	int[][] pel=new int[n][n];
+	for(int i=0; i<n; i++) {
+		pel[i][i]=1;
 	}
+		
+	for(int i=1; i<n; i++) {
+		System.out.println(input.charAt(i)+" 시작");
+		for(int j=i-1; j>=0; j--) { // 팰린드롬 검사
+			if(pel[i-1][j]==1) {
+				System.out.println("펠린드롬");
+				int side = (i-1)-(i-j);
+				
+				System.out.println("side:"+side);
+				if(side>=0 && input.charAt(i)==input.charAt(side)) {
+					pel[i][side] = 1;
+				}
+			}
+		}
+		
+		for(int j=i-1; j>=0; j--) {
+			if(input.charAt(i)!=input.charAt(j)) {
+				break;
+			}
+			pel[i][j]=1;
+		}
+		
+		for(int i=0; i<n; i++) {
+			System.out.println(Arrays.toString(pel[i]));
+		}
+		
+		
+		dp[0]=1;
+		for(int i=1; i<n; i++) {
+			System.out.println(input.charAt(i)+" dp 시작");
+			
+			for(int j=i-1; j>=0; j--) { // 0 ~ i-1
+				if(pel[i][j]==1) {
+					System.out.println("pelindrom j:"+j);
+					dp[i]= Math.min(dp[i-1]+1, (j-1<0?0:dp[j-1])+1); 
+					
+				}
+			}
+			if(dp[i]==0) dp[i]=dp[i-1]+1;
+			System.out.println(Arrays.toString(dp));
+		}
+		//System.out.println("dp check");
+		System.out.println(dp[n-1]);
+	}
+
 
 }
